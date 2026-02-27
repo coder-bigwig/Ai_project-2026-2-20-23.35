80e24dadf4dfff005bf16ddd38ef55b73b996a7e
coder-bigwig <2875018350@qq.com>
2026-02-27 18:14:47 +0800

    chore: remove pack_check artifacts and ignore directory

--- _pack_check/backend/app/repositories/submissions.py
+++
@@ -1,5 +0,0 @@
-from .student_experiments import StudentExperimentRepository
-
-
-class SubmissionRepository(StudentExperimentRepository):
-    """Backward-compatible alias repository for student experiment submissions."""
